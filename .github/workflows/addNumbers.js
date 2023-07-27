63a0cb3e89680f8c6673cc165108e7d168aab08b
Sajan Nair <snair@goempyrean.com>
2023-07-27 12:10:50 -0500

    Delete addNumbers.js

--- .github/workflows/addNumbers.js
+++
@@ -1,6 +0,0 @@
-const addNumbers= (a,b) => a + b
-
-const firstNumber = parseInt(process.argv[2]);
-const secondNumber = parseInt(process.argv[3]);
-const sum = addNumbers(firstNumber, secondNumber);
-console.log(sum);
